67a7f576c594f4c47b9dfd9ff42c00f83ea17aa3
Bryan <git@singee.me>
2024-09-06 17:29:48 +0800

    add 2024-09

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import { Hono } from 'hono'
-import { HTTPException } from 'hono/http-exception';
-import { errorToResponse, HTTP404 } from './error';
+import {Hono} from 'hono'
+import {HTTPException} from 'hono/http-exception';
+import {errorToResponse, HTTP404} from './error';
 
-import { generateICS } from './ics';
+import {generateICS} from './ics';
 import events202408 from "../events/2024-08.json";
+import events202409 from "../events/2024-09.json";
 
 
 const app = new Hono()
 
 app.onError((error, _c) => {
-  if (error instanceof HTTPException) {
-    return error.getResponse();
-  }
+    if (error instanceof HTTPException) {
+        return error.getResponse();
+    }
 
-  return errorToResponse(error);
+    return errorToResponse(error);
 });
 app.notFound(() => {
-  throw HTTP404;
+    throw HTTP404;
 });
 
 app.get('/', (c) => c.text('Contact Bryan (contact.lme-schedule-server@singee.me) for help'))
 
 app.get('/ics', async (c) => {
-  const url = new URL(c.req.url)
-  const icsString = await generateICS(events202408 as any, url.search);
+    const url = new URL(c.req.url)
+    const icsString = await generateICS([...events202408 as any, ...events202409 as any], url.search);
 
-  return c.text(icsString)
+    return c.text(icsString)
 })
 
 export default app
