c5ead71a32e51e7391d96fe9a198d250eaff3c43
Bryan <git@singee.me>
2024-11-17 19:48:46 +0800

    add /links

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {HTTPException} from 'hono/http-exception';
 import {errorToResponse, HTTP404} from './error';
 
 import indexHtml from './index.html'
+import linksHtml from './links.html'
 
 import {generateICS} from './ics';
 import { getEvents } from './events';
@@ -24,6 +25,21 @@ app.notFound(() => {
 });
 
 app.get('/', (c) => c.html(indexHtml))
+app.get('/links', (c) => c.html(linksHtml))
+
+app.get('/links.json', async (c) => {
+    const response = await fetch('https://jsonbin.singee.workers.dev/lme-box', {
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwM2N1bnp1ZXdodjl1cGhlZmVvMWFwYXFvIiwiaWF0IjoxNzMxODQzNTcwLCJhIjp7ImxtZS1ib3giOiJyIn19.GVs4VE-tZFjYoyWDmMfiuD4AhaTq9C22mHgss-NlMY4'
+        }
+    })
+
+    const result = await response.json<unknown[]>();
+
+    const messages = result.reverse().slice(0, 50)
+    
+    return c.json(messages)
+})
 
 function getFilteredEvents(url: string) {
     const filter = new URL(url).search
